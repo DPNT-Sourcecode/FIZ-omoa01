2d6514ff475c303e4e43bd221a86e356400f71fa
sbx_user1051 <sbx_user1051@169.254.172.121>
2023-02-17 15:34:54 +0000

    Fri Feb 17 15:34:54 UTC 2023

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -7,15 +7,17 @@ public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
         String answer = "";
-       if(number%3==0)
-       {
-           answer+="fizz";
-           if(String.valueOf(number).contains("3"))
-           {
-               if()
-           }
-       }
-
+        if (number % 3 == 0 || String.valueOf(number).contains("3")) {
+            answer += "fizz ";
+        }
+        if (number % 5 == 0 || String.valueOf(number).contains("5")) {
+            answer += "buzz ";
+        }
+        if(number % 3 == 0 && String.valueOf(number).contains("3")||number % 5 == 0 || String.valueOf(number).contains("5")){
+            if(number%2==1)
+                answer+="fake ";
+            answer+="deluxe ";
+        }
         if (answer.equals(""))
             return String.valueOf(number);
         else
@@ -23,5 +25,6 @@ public class FizzBuzzSolution {
     }
 
 }
+
 
 
