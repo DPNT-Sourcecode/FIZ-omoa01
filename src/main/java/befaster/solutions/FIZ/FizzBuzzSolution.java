5c6f00e142131a158198bcd155cdda11ff2a525e
sbx_user1051 <sbx_user1051@169.254.172.121>
2023-02-17 15:13:24 +0000

    Fri Feb 17 15:13:24 UTC 2023

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -6,7 +6,7 @@ import com.sun.org.apache.xpath.internal.objects.XNull;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        String answer = null;
+        String answer = "";
         if (number % 3 == 0) {
             answer += "fizz ";
         }
@@ -14,12 +14,10 @@ public class FizzBuzzSolution {
             answer += "buzz ";
         }
 
-        if (answer == null)
+        if (answer.equals(""))
             return String.valueOf(number);
         else
             return answer.trim();
     }
 
 }
-
-
--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -8,6 +8,3 @@ public class HelloSolution {
         return String.format("Hello, %s!",friendName);
     }
 }
-
-
-
