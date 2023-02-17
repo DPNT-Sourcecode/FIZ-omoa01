04060aa3e526d772ded55ffa6d3f51136f6984e6
sbx_user1051 <sbx_user1051@169.254.172.121>
2023-02-17 15:12:01 +0000

    Fri Feb 17 15:12:01 UTC 2023

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -1,11 +1,14 @@
 package befaster.solutions.FIZ;
 
 import befaster.runner.SolutionNotImplementedException;
+import com.sun.org.apache.xpath.internal.objects.XNull;
 
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
-        throw new SolutionNotImplementedException();
+
+        if(number%)
     }
 
 }
+
