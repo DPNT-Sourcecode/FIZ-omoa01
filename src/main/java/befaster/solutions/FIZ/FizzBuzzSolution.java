a045ece9fe5186dd5a6541f226ffcfca3d032eb7
sbx_user1051 <sbx_user1051@169.254.172.121>
2023-02-17 15:12:51 +0000

    Fri Feb 17 15:12:51 UTC 2023

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -6,9 +6,20 @@ import com.sun.org.apache.xpath.internal.objects.XNull;
 public class FizzBuzzSolution {
 
     public String fizzBuzz(Integer number) {
+        String answer = null;
+        if (number % 3 == 0) {
+            answer += "fizz ";
+        }
+        if (number % 5 == 0) {
+            answer += "buzz ";
+        }
 
-        if(number%)
+        if (answer == null)
+            return String.valueOf(number);
+        else
+            return answer.trim();
     }
 
 }
+
 
